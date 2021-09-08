f426d122257ee259b488200e0fffc6a790efa8a0
Yuvraj Singh <zyuvraj53@gmail.com>
2021-09-08 15:29:03 +0530

    .

--- Fireworks/particle.js
+++ Fireworks/particle.js
@@ -1,12 +1,12 @@
 function Particle(x, y) {
   this.pos = createVector(x, y);
-  this.vel = createVector(0, 0);
+ this.vel = createVector(0, -10);
   this.acc = createVector(0, 0);
 
   this.applyForce = function (force) {
     this.acc.add(force);
   };
-  
+
   this.update = function () {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
--- Fireworks/sketch.js
+++ Fireworks/sketch.js
@@ -1,15 +1,18 @@
 var firework;
+var gravity;
 
 function setup() {
   createCanvas(400, 400);
+  gravity = createVector(0, 0.2);
   stroke(255);
   strokeWeight(4);
   noFill();
-  firework = new Particle(200, 150);
+  firework = new Particle(random(width), height);
 }
 
 function draw() {
   background(125, 0, 255);
+  firework.applyForce(gravity);
   firework.update();
   firework.show();
 }
