b52469b56596b609709bc40e06be31af7230637d
Yuvraj Singh <zyuvraj53@gmail.com>
2021-09-09 16:37:31 +0530

    .

--- Fireworks/sketch.js
+++ Fireworks/sketch.js
@@ -1,18 +1,19 @@
-var firework;
+var fireworks = [];
 var gravity;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
   gravity = createVector(0, 0.2);
   stroke(255);
   strokeWeight(4);
   noFill();
-  firework = new Particle(random(width), height);
 }
 
 function draw() {
   background(125, 0, 255);
-  firework.applyForce(gravity);
-  firework.update();
-  firework.show();
+  fireworks.push(new Firework());
+  for (let i = 0; i < fireworks.length; i++) {
+    fireworks[i].update();
+    fireworks[i].show();
+  }
 }
